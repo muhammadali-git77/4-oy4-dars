7bcdf5584ed39934a9efbe1a5136a388a508fe45
Muhammadali Abdumannonov <aalixon636@gmail.com>
2025-04-30 10:27:17 +0500

    Vazifa yakunlandi

---
+++ app.js
@@ -0,0 +1,90 @@
+// const number1 = +prompt("Number 1");
+// const number2 = +prompt("Number 2");
+// const amal = prompt("Amalni kiriting:");
+
+// if (amal == "+") {
+//   alert(number1 + number2);
+// } else if (amal == "-") {
+//   alert(number1 + number2);
+// } else if (amal == "/") {
+//   alert(number1 / number2);
+// } else if (amal == "*") {
+//   alert(number1 * number2);
+// } else if (amal == "%") {
+//   alert(number1 % number2);
+// } else if (amal == "**") {
+//   alert(number1 ** number2);
+// } else {
+//   alert("Bunday amal yo'q! Siz: +, -, *,**, /, % kabi amallarni kirta olasz.");
+// }
+
+// switch (amal) {
+//   case "+":
+//     alert(number1 + number2);
+//     break;
+//   case "-":
+//     alert(number1 - number2);
+//     break;
+//   case "*":
+//     alert(number1 * number2);
+//     break;
+//   case "**":
+//     alert(number1 ** number2);
+//     break;
+//   case "/":
+//     alert(number1 / number2);
+//     break;
+//   case "%":
+//     alert(number1 % number2);
+//     break;
+
+//   default:
+//     alert(
+//       "Bunday amal yo'q! Siz: +, -, *,**, /, % kabi amallarni kirta olasz."
+//     );
+//     break;
+// }
+
+// for (let index = 0; index < 10; index++) {
+//   if (index % 2 == 1) {
+//     console.log(index );
+//   } else {
+//   }
+// }
+
+// let count = 0;
+// let son = 13;
+// let num = 0;
+// for (let index = 1; index < son; index++) {
+//   if (son % index == 0) {
+//     console.log(index);
+//     num++;
+//   }
+// }
+
+// console.log()
+
+// let k = 2;
+// let n = 3;
+// if (n > 0) {
+//   for (let i = 0; i < n; i++) {
+//     console.log(k);
+//   }
+// }
+
+// let narx = 12000;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log((i / 10) * narx);
+// }
+
+// let a = 10;
+// let b = 17;
+// let counter = 0;
+
+// for (let i = b; i >= a; i--) {
+//   counter++;
+//   console.log(i);
+// }
+
+// console.log(counter);
---
+++ homework.js
@@ -0,0 +1,90 @@
+// For1. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b ni ham) chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi programma tuzilsin. (a va b xam chiqarilsin).
+// let a = 1;
+// let b = 10;
+// let count = 0;
+
+// for (let i = a; i <= b; i++) {
+//   count++;
+//   console.log(i);
+// }
+// console.log(`A va B sonlari soni ${count} ta songa teng.`);
+
+// For2. a va b butun sonlari berilgan (a < b). a va b sonlari orasidagi barcha butun sonlarni (a va b dan tashqari) kamayish tartibida chiqaruvchi va chiqarilgan sonlar sonini chiqaruvchi progma tuzilsin.
+// let a = 1;
+// let b = 10;
+// let count = 0;
+
+// for (let i = b - 1; i > a; i--) {
+//   count++;
+//   console.log(i);
+// }
+// console.log(`A va B sonlari oraligi ${count} ta songa teng.`);
+
+// For3. Bir kg konfetning narxi berilgan (haqiqiy son). 1, 2, 10 kg konfetni narxini chiqaruvchi programma tuzilsin.
+// let price = 10000;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i * price);
+// }
+
+// For4. Bir kg konfetning narxi berilgan (haqiqiy son). 1.2, 1.4, ..., 2 kg konfetni narxini chiqaruvchi programma tuzilsin.
+// let price = 10000;
+
+// for (let i = 10; i <= 20; i++) {
+//   console.log((i / 10) * price);
+// }
+
+// For5. a va b butun sonlari berilgan (a < b). a dan b gacha bo'lgan barcha butun sonlar yig'indisini chiqaruvchi programma tuzilsin.
+// let a = 1;
+// let b = 32;
+// let count = 0;
+
+// for (let i = a; i <= b; i++) {
+//   count += i;
+// }
+
+// console.log(count);
+
+// For6. a va b butun sonlari berilgan (a < b). a dan b gacha bo'lgan barcha butun sonlar ko'paytmasini chiqaruvchi programma tuzilsin.
+// let a = 1;
+// let b = 32;
+// let count = 1;
+
+// for (let i = a; i <= b; i++) {
+//   count *= i;
+// }
+
+// console.log(count);
+
+// For7. a va b butun sonlari berilgan (a < b). a dan b gacha bo'lgan barcha butun sonlar kvadratlarining yig'indisini chiqaruvchi programma tuzilsin.
+// let a = 1;
+// let b = 32;
+// let count = 0;
+
+// for (let i = a; i <= b; i++) {
+//   count += i ** 2;
+// }
+
+// console.log(count);
+
+// For8. n butun soni berilgan (n > 0). Quyidagi yig'indini hisoblovchi programma tuzilsin. S=1+1/2+1/3+...+1/n
+// let n = 20;
+// let count = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   count += 1 / i;
+//   //   console.log(1 / i);
+// }
+
+// console.log(count);
+
+// For9. n butun soni berilgan (n > 0). Quyidagi ko'paytmani hisoblovchi programma tuzilsin. S=1.1*1.2* 1.3*... *n
+// let n = 30;
+// let count = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   //   console.log(i / 10);
+//   count *= i / 10;
+// }
+
+// console.log(count);
